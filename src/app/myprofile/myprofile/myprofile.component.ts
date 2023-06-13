4c20b88cb032b97b0bf1dcfe25ba66677ceb5a5e
vignesh-msc <vinagarajanmsc@gmail.com>
2023-06-13 17:13:50 +0530

    changes

--- src/app/myprofile/myprofile/myprofile.component.ts
+++ src/app/myprofile/myprofile/myprofile.component.ts
@@ -12,8 +12,12 @@ export class MyprofileComponent {
 
  // ProfilecreationService
  empprofile: Employeeprofile;
+ empprofile_edit:Employeeprofile;
+ title="";
+ selctededitdetials='';
   keys :string[] = [];
   isLoading:boolean = false;
+  display: string = "none";
 // Object.keys(officialDetails);
  constructor(private auth:authservice,private pf:ProfilecreationService){
   
@@ -23,6 +27,7 @@ export class MyprofileComponent {
 this.pf.getProfileDetails(this.auth.getUserID()).subscribe({
   next: (response) => {
    this.empprofile = response;
+   this.empprofile_edit = response;
     console.log('response',this.empprofile);
     this.keys = Object.keys(this.empprofile);
     this.isLoading = false;
@@ -33,6 +38,30 @@ this.pf.getProfileDetails(this.auth.getUserID()).subscribe({
   },
 });
  
+ }
+ onCloseHandled(){
+  this.display = "none";
+ }
+ openModal(data: any,type:string){
+
+  switch (type) {
+    case 'Edq':
+      this.selctededitdetials ='educationalqualification';
+  this.title ="Edit Educational Details"
+      break;
+    case 'StatDtls':
+      this.selctededitdetials ='statutorydetails';
+      this.title ="Edit Statutory Details"
+      break;
+    case 'Bankdtls':
+      this.selctededitdetials ="bankdetails";
+      this.title ="Edit Bank Details"
+      break;
+    default:
+      this.title ="Edit Details"
+      break;
+  }
+  this.display = "block";
  }
 
 }
